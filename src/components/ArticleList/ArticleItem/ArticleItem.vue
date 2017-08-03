05315dde0e944a56bde70dca0bb49315ed15d845
BadWaka <1456683844@qq.com>
2017-08-03 10:44:43 +0800

    增加下划线

--- src/components/ArticleList/ArticleItem/ArticleItem.vue
+++ src/components/ArticleList/ArticleItem/ArticleItem.vue
@@ -7,6 +7,9 @@
             </section>
             <div class="imgWrapper" style="background-image:url(http://img.hb.aicdn.com/f3bbb6785e0c2b4d14a7d8c9b78be705cc3e1e6419aad-b48HEq_fw658)"></div>
         </section>
+        <section class="row2">
+            <div class="tag">标签</div>
+        </section>
     </section>
 </template>
 
@@ -23,6 +26,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang='scss'>
+@import '../../../common/color.scss';
+
 .articleItem {
     position: relative;
     margin-top: 10px;
@@ -42,11 +47,21 @@ export default {
         .left {
 
             .title {
+                display: inline-block;
+                color: $darkBlack;
+                transition: 0.3s;
                 font-size: 18px;
                 font-weight: bold;
+                border-bottom: 1px solid #ffffff;
+                cursor: pointer;
+
+                &:hover {
+                    border-bottom: 1px solid $darkBlack;
+                }
             }
 
             .desc {
+                color: $lightBlack;
                 margin-top: 10px;
                 font-size: 14px;
                 line-height: 20px;
@@ -60,6 +75,25 @@ export default {
             height: 100px;
             background: no-repeat center;
             background-size: cover;
+        }
+    }
+
+    .row2 {
+        display: flex;
+
+        .tag {
+            transition: 0.3s;
+            padding: 3px 5px;
+            border: 1px solid $blue500;
+            border-radius: 2px;
+            font-size: 12px;
+            color: $blue500;
+            cursor: pointer;
+            user-select: none;
+
+            &:hover {
+                background: $blue50;
+            }
         }
     }
 }
