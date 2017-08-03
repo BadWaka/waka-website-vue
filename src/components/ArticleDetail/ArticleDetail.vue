0c3ee4780ce663671b2d2f6820350e765eb35cea
BadWaka <1456683844@qq.com>
2017-08-03 18:19:26 +0800

    换个思路吧

---
+++ src/components/ArticleDetail/ArticleDetail.vue
@@ -0,0 +1,30 @@
+<template>
+    <!-- 文章详情 -->
+    <section class="articleDetail">
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'articleDetail',
+    data() {
+        return {
+            title: '文章详情'
+        }
+    },
+    // 组件
+    components: {
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang='scss'>
+@import '../../common/base.scss';
+.articleDetail {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    box-sizing: border-box;
+}
+</style>
