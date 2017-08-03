47c74262b9cf301ed4554c7077fda3d1c9c8fccf
BadWaka <1456683844@qq.com>
2017-08-03 15:56:38 +0800

    使用vue-awesome-swiper

--- src/components/Index/Index.vue
+++ src/components/Index/Index.vue
@@ -3,8 +3,8 @@
     <!-- 头部 -->
     <m-header></m-header>
     <swiper class="swiper" :options="swiperOption" ref="mySwiper">
-      <!-- slides -->
-      <swiper-slide class="swiperItem">I'm Slide 1</swiper-slide>
+      <!-- 轮播图 -->
+      <swiper-slide v-for="(carousel,index) in carousels" :key="index" class="swiperItem" :style="{backgroundImage:'url('+carousel.imgUrl+')'}"></swiper-slide>
     </swiper>
     <!-- 文章列表 -->
     <article-list></article-list>
@@ -26,13 +26,11 @@ export default {
       swiperOption: {
       },
       carousels: [{
-        imgUrl: 'http://img.hb.aicdn.com/a1027848e343fa6b9028d4f21d94ad104a3d31978c7cf-hEDM2n_fw658',
+        imgUrl: 'http://img.hb.aicdn.com/b895d7c0a875555aa5ee717982eb3b4ef3e358c83ce3a-DbZSpf_fw658',
       }, {
-        imgUrl: 'http://img.hb.aicdn.com/0807a0f7f9e654207dd5890c32cf45740b05462d16f5d-f64rIU_fw658',
+        imgUrl: 'http://img.hb.aicdn.com/d65a58ef3c783940bbf74049c985527142323f0d2c12e-Gk6mMA_fw658',
       }, {
-        imgUrl: 'http://img.hb.aicdn.com/3dfc9cbc0968537879ee32c5d1fcfaeae317301d19e9a-Y6HDN6_fw658',
-      }, {
-        imgUrl: 'http://img.hb.aicdn.com/3dfc9cbc0968537879ee32c5d1fcfaeae317301d19e9a-Y6HDN6_fw658',
+        imgUrl: 'http://img.hb.aicdn.com/3be040f4bcc088e6ca90de1c02fa3a86f4bc00ad812f8-5gVpRb_fw658',
       }],
     }
   },
@@ -59,7 +57,7 @@ export default {
 
   .swiper {
     margin-top: 20px;
-    width: 100%;
+    width: $containerWidth;
     background: yellow;
 
     .swiperItem {
@@ -67,7 +65,6 @@ export default {
       height: 300px;
       background: no-repeat center;
       background-size: cover;
-      background: red;
     }
   }
 }
